c3ce89f36133d2ab3fe1ac4658dbd5d4e4ed6b31
Anuja-Bhimraj-Pawar <anuja13122004@gmail.com>
2025-05-28 23:00:35 +0530

    Navigation through Names

--- First_Project/src/App.jsx
+++ First_Project/src/App.jsx
@@ -1,47 +1,67 @@
-// import React, { use, useState } from 'react';
-
-// const App = () => {
-//   // const user = "Anuja";
-//   const [user, setUser] = useState("Anuja")
-  
-//   const handleBtn = () => {
-//     setUser("Aryan");
-//   }
-
-
-//   return (
-//     <>
-//       <h1>Username: {user}</h1>
-//       <button onClick={handleBtn}>Change the Username</button>
-//     </>
-//   )
-// }
-
-// export default App;
-
-import React, { useState } from 'react'
+import React, { use, useState } from 'react';
 
 const App = () => {
+  // const user = "Anuja";
+  // const [user, setUser] = useState("Anuja");
+  // const names = ["Aryan", "Vinish", "Sparsh", "Riyansh", "Saksham"];
+  // const handleBtn = () => {
+  //   const randomName = names[Math.floor(Math.random() * names.length)];
+  //   setUser(randomName);
+  // }
 
-  const [count, setCount] = useState(0)
+  // Forward Navigation 
+  const names = ["Anuja", "Aryan", "Vinish", "Sparsh", "Saksham"];
+  const [index, setIndex] = useState(0)
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  const handleBtn = () => {
+    setIndex((prevIdx) => (prevIdx + 1) % names.length);
   }
 
-  const handleDecrement = () => {
-    setCount(count - 1);
-  }
+  // Backward Navigation
+  // const names = ["Anuja", "Aryan", "Vinish", "Sparsh", "Saksham"];
+  // const [index, setIndex] = useState(names.length-1);
+
+  // const handleBtn = () => {
+  //   setIndex((prevIdx) => (prevIdx - 1 + names.length) % names.length);
+  // };
+
 
   return (
     <>
-      <div id='countBtn'>Counter: {count}</div>
-      <div id="btns">
-        <button id='incrementBtn' onClick={handleIncrement}>Increment (+)</button>
-        <button id='decrementBtn' onClick={handleDecrement}>Decrement (-)</button>
-      </div>
+      <h1>Username: {names[index]}</h1>
+      <button onClick={handleBtn}>Change the Username</button>
     </>
   )
 }
 
-export default App
+export default App;
+
+
+
+// Counter Project
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   const [count, setCount] = useState(0)
+
+//   const handleIncrement = () => {
+//     setCount(count + 1);
+//   }
+
+//   const handleDecrement = () => {
+//     setCount(count - 1);
+//   }
+
+//   return (
+//     <>
+//       <div id='countBtn'>Counter: {count}</div>
+//       <div id="btns">
+//         <button id='incrementBtn' onClick={handleIncrement}>Increment (+)</button>
+//         <button id='decrementBtn' onClick={handleDecrement}>Decrement (-)</button>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
